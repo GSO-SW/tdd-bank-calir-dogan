41c634710ece87eecfcb854a5a417324b4e97fdd
Ertuerk Dogan <ertuerk.d2@gso.schule.koeln>
2023-09-25 10:57:44 +0200

    Kontostand stimmt nach Auszahlung

---
+++ KontoTest/Auszahltest.cs
@@ -0,0 +1,24 @@
+﻿using Bank;
+namespace Auszahltest
+{
+    [TestClass]
+    public class Auszahltest
+    {
+        [TestMethod]
+        public void guthaben_stimmt_nach_auszahlung()
+        {
+            int startKapital = 200;
+
+            Konto auszahlTest = new Konto(startKapital);
+
+            int endWert = 50;
+            int auszahlung = 150;
+
+            auszahlTest.Auszahlen(auszahlung);
+
+            Assert.AreEqual(endWert, auszahlTest.Guthaben);
+
+
+        }
+    }
+}
