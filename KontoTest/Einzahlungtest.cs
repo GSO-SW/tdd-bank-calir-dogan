5cce4fdd7d665330c44ce6e2db45b0d838f25c13
Ertuerk Dogan <ertuerk.d2@gso.schule.koeln>
2023-09-25 10:49:56 +0200

    Kontoguthaben stimmt nach mehreren Einzahlungen noch

---
+++ KontoTest/Einzahlungtest.cs
@@ -0,0 +1,22 @@
+﻿using Bank;
+
+namespace Einzahlungstest
+{
+    [TestClass]
+    public class Einzahlungtest
+    {
+        [TestMethod]
+        public void nachn_einzahlung_betrag_prüfen()
+        {
+            int startKapital = 0;
+            Konto einzahlungTest = new Konto(startKapital);
+
+            int einzahlungGesamt = 100;
+            int einzahlung = 50;
+            einzahlungTest.Einzahlen(einzahlung);
+            einzahlungTest.Einzahlen(einzahlung);
+
+            Assert.AreEqual(einzahlungGesamt, einzahlungTest.Guthaben);
+        }
+    }
+}
